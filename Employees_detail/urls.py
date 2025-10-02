35631961e70de643e9f58c4817d4c2982252bba8
Umesh Badsara <72116317+Coder-badsara@users.noreply.github.com>
2025-10-02 14:37:20 +0530

    Add edit_employee path to URLs

--- Employees_detail/urls.py
+++ Employees_detail/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('<int:emp_id>/', views.employee_info, name="employee_info"),
     path('add/', views.add_employee, name="add_employee"),
     path('<int:emp_id>/delete/', views.employee_delete , name = 'employee_delete'),
+    path('<int:emp_id>/edit/', views.edit_employee, name="edit_employee"),
 
     
+
 ]
